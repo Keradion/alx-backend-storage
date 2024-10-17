90775d7490e6a12dcaece901a96e67eba25f7cb9
Daniel <danielshitaye10@gmail.com>
2024-10-17 12:55:41 +0000

    create table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Define a table called users in a database. 
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL,
+	name VARCHAR(255),
+	PRIMARY KEY(id), 
+	UNIQUE(email)
+);
