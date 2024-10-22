63fce00f602122c49031fd676823e21f6ae6e929
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-22 06:35:47 -0700

    update document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Update all topics of a school document """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Update a document in mongo_collection with name name to the
+    value topics
+    """
+    # Filter to identify the document to be update
+    query_filter = {'name': name}
+    # Document key and new value to be update
+    update_operation = {'$set': {'topics': topics}}
+    # updating document ...
+    mongo_collection.update_one(query_filter, update_operation)
