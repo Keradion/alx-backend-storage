b342ee1c002bf1b06f169ac524d9e4776d0722eb
Your Name <you@example.com>
2024-10-23 19:49:30 +0000

    Redis instance

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' Writing strings to Redis '''
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    def __init__(self):
+        '''
+           Create Redis instance
+        '''
+        # Redis instance db __redis created
+        self.__redis = redis.Redis()
+        # Delete all keys in the current Redis instance db __redis
+        self.__redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        ''' Takes data argument and generate a random key
+            store the data in redis using the key and return the key
+        '''
+        # Generating a random key using uuid
+        random_key = str(uuid.uuid4())
+
+        # Storing the data on redis db using random_key as key
+        self.__redis.set(random_key, data)
+
+        return random_key
