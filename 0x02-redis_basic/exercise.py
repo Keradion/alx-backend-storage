6a8893809c54462cbfd31dab85d95d311b822173
Your Name <you@example.com>
2024-10-23 19:56:58 +0000

    Redis instance

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-''' Writing strings to Redis '''
+""" Writing strings to Redis database key value pair
+"""
 import redis
 import uuid
 from typing import Union
@@ -7,18 +8,19 @@ from typing import Union
 
 class Cache:
     def __init__(self) -> None:
-        '''
-           Create Redis instance
-        '''
+        """
+           Create Redis instance and clear existing key value pairs
+        """
         # Redis instance db __redis created
         self.__redis = redis.Redis()
         # Delete all keys in the current Redis instance db __redis
         self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        ''' Takes data argument and generate a random key
+        """
+            Takes data argument and generate a random key
             store the data in redis using the key and return the key
-        '''
+        """
         # Generating a random key using uuid
         random_key = str(uuid.uuid4())
 
