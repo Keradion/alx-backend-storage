796860a99bc75ee04bc032fdff627e9e44be9384
Your Name <you@example.com>
2024-10-25 20:26:52 +0000

    clean

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-   working with redis 
+   working with redis
 """
 import redis
 import uuid
@@ -12,10 +12,10 @@ def count_calls(method: Callable) -> Callable:
     """ Decorater that takes a single method and returns a callable """
     @wraps(method)
     def increment_key_value(self: Any, *args, **kwargs) -> str:
-        """ Wraps store cache function and Increment the value associated with key
-            every time store function is called
+        """ Wraps store cache function and Increment the
+            value associated with key every time store function is called
         """
-        self._redis.incr(method.__qualname__)  # Increment the value of the key by 1
+        self._redis.incr(method.__qualname__)  # Increment key value
         return method(self, *args, **kwargs)
     return increment_key_value
 
@@ -32,7 +32,7 @@ def call_history(method: Callable) -> Callable:
         # Appending the input parameter inside inputs redis list
         self._redis.rpush(f'{method.__qualname__}:inputs', str(args))
 
-        return_value  = method(self, *args)
+        return_value = method(self, *args)
 
         # Appending the return value of catch.store inside output redis list
         self._redis.rpush(f'{method.__qualname__}:outputs', return_value)
@@ -53,9 +53,8 @@ class Cache:
         # Delete all keys in the current Redis instance db __redis
         self._redis.flushdb()
 
-    
-    @call_history
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
             Takes data argument and generate a random key
@@ -69,22 +68,22 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
-    
+
     def get(self, key: str, fn: callable = None) -> Any:
+        """ call appropirate method of conversion """
         # Fetching the value associated with key in redis
         value: str = self._redis.get(key)
         if not value:
-            return 
+            return
         if fn is int:
             return self.get_int(value)
         if fn is str:
             return self.get_str(value)
         return value
 
-
     def get_str(self, value: bytes) -> str:
         """
-           Converts byte to string 
+           Converts byte to string
         """
         return value.decode('utf-8')
 
@@ -93,12 +92,17 @@ class Cache:
            Converts byte to integer
         """
         return int(value)
-    
+
     def replay(self):
         """ Print number of times a method called with key and value """
-        print("Catch.store was called {} times:".format(len(self.store.__qualname__)))
-        inputs = self._redis.lrange("{}:inputs".format(self.store.__qualname__), 0, -1)
-        outputs = self._redis.lrange("{}:outputs".format(self.store.__qualname__), 0, -1)
+        print("Catch.store was called {} times:".format(
+            len(self.store.__qualname__)))
+        inputs = self._redis.lrange("{}:inputs".format(
+            self.store.__qualname__), 0, -1)
+        outputs = self._redis.lrange("{}:outputs".format(
+            self.store.__qualname__), 0, -1)
         input_output = list(zip(inputs, outputs))
         for key_value_pair in input_output:
-            print("Cache.store(*{}) -> {}".format(key_value_pair[0].decode('utf-8'), (key_value_pair[1]).decode('utf-8')))
+            print("Cache.store(*{}) -> {}".format(
+                key_value_pair[0].decode(
+                    'utf-8'), (key_value_pair[1]).decode('utf-8')))
