6c02597a69f556fb4e3aaebd9f51847eac117d0f
Your Name <you@example.com>
2024-10-27 08:54:38 +0000

    catch

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,51 @@
 #!/usr/bin/env python3
 """
-Caching request module
+   Implementing an expiring web cache and tracker
 """
 import redis
 import requests
+from typing import Callable, Any
 from functools import wraps
-from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
+def count_url(method: Callable) -> Callable:
     """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+        Decorator to track how many times a URL accessed
+        and cache the result for 10 sec with expiration.
+        Track the frequency using "count:{url}" as a key.
+    """
+    @wraps(method)
+    def wrapper(url: str) -> Callable:
+        """ """
+        url = str(url)
+        redis_client = redis.Redis()
+        url_count_key = 'count:{}'.format(url)
+        url_cache_key = '{}'.format(url)
+        redis_client.incr(url_count_key)
+        
+        html_content = method(url)
+        # Check if the URL html content is already in the cache
+        url_exist = redis_client.get(url_cache_key)
+
+        # If the url html content is not in the cache, store it for 10 sec.
+        if not url_exist:
+            redis_client.setex(url_cache_key, 10, html_content)
+            print(redis_client.ttl(url_cache_key))
+        return html_content
     return wrapper
 
+ 
 
-@track_get_page
+
+
+@count_url
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
-    """
-    response = requests.get(url)
-    return response.text
+    """ Fetch html content of a url using request and return it """
+    html_page = requests.get(url)
+    return html_page.content
+
+
+if __name__ == '__main__':
+    result  = get_page('http://slowwly.robertomurray.co.uk')
+    print(result)
+
