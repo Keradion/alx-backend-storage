64224e515770775883e2ea07c9a2537a05b9d3cf
Your Name <you@example.com>
2024-10-27 09:34:31 +0000

    clean

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,6 +9,7 @@ from functools import wraps
 
 redis_client = redis.Redis()
 
+
 def count_url(method: Callable) -> Callable:
     """
         Decorator to track how many times a URL accessed
@@ -19,17 +20,14 @@ def count_url(method: Callable) -> Callable:
     def wrapper(url: str) -> str:
         """ """
         url = str(url)
-        # Redis keys 
+        # Redis keys
         url_count_key = 'count:{}'.format(url)
         url_cache_key = '{}'.format(url)
         redis_client.incr(url_count_key)
-        
         # Check if the URL html content is already in the cache
         html_content = redis_client.get(url_cache_key)
-        
         if html_content:
             return html_content.decode('utf-8')
-        
         # If the url html content is not in the cache, store it for 10 sec.
         html_content = method(url)
         redis_client.setex(url_cache_key, 10, (html_content))
@@ -42,4 +40,3 @@ def get_page(url: str) -> str:
     """ Fetch html content of a url using request and return it """
     html_page = requests.get(url)
     return html_page.text
-
